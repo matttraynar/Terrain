d8e77a0d948bacfd807eed8133c67592c97e675c
Matt Traynar <matt.traynar@gmail.com>
2017-02-08 13:48:50 +0000

    Perline noise generator. Also has capability to change octaves and persistence of the function

--- include/perlinnoise.h
+++ include/perlinnoise.h
@@ -1,4 +1,29 @@
 #ifndef PERLINNOISE_H
 #define PERLINNOISE_H
 
-#endif // PERLINNOISE_H
+#include <vector>
+#include <cmath>
+#include <random>
+#include <algorithm>
+#include <numeric>
+
+class PerlinNoise
+{
+public:
+    // Initialize with the reference values for the permutation vector
+    PerlinNoise();
+
+    // Get a noise value, for 2D images z can have any value
+    double noise(double x, double y, double z);
+    double octaveNoise(double x, double y, double z, int octaves, double persistence);
+
+private:
+    double fade(double t);
+    double grad(int hash, double x, double y, double z);
+    double lerp(double a, double b, double t);
+
+    // The permutation vector
+    std::vector<int> p;
+};
+
+#endif
