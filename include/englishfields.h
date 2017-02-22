48b160cb14bd92e6d8c3daaf13f82bfc9b422126
Matt Traynar <matt.traynar@gmail.com>
2017-02-22 18:02:44 +0000

    Set up the field generator class

--- include/englishfields.h
+++ include/englishfields.h
@@ -1,13 +1,41 @@
 #ifndef ENGLISHFIELDS_H
 #define ENGLISHFIELDS_H
 
+#include <QVector3D>
+
+#include <vector>
+#include <memory>
+
+struct Line
+{
+    inline Line(QVector3D a, QVector3D b) : p0(a), p1(b) {}
+
+    inline float getLength() const { return (p1 - p0).length(); }
+
+    QVector3D p0;
+    QVector3D p1;
+};
+
 class EnglishFields
 {
 public:
-    EnglishFields();
+    EnglishFields(const std::vector<std::vector<float> >& _terrainHeightMap);
     ~EnglishFields();
 
+    void checkAvailableSpace();
+    void threeFieldModel();
+
+    inline std::vector<Line> getFieldBoundary() const { return m_fieldBoundary; }
+    inline std::vector< std::vector<Line> > getFields() const { return m_fields; }
+
+
 private:
+    EnglishFields();
+
+    std::shared_ptr< std::vector<std::vector<float> > > m_heightMapCopy;
+    std::vector<Line> m_fieldBoundary;
+    std::vector< std::vector<Line> > m_fields;
+
 
 };
 
--- src/englishfields.cpp
+++ src/englishfields.cpp
@@ -4,3 +4,29 @@ EnglishFields::EnglishFields()
 {
 
 }
+
+EnglishFields::EnglishFields(const std::vector<std::vector<float> > &_terrainHeightMap)
+{
+    //Point the local shared pointer to the terrain height map reference, looks horrible
+    //but thats because the pointer type is vector<vector<float>>
+    m_heightMapCopy = std::make_shared< std::vector< std::vector<float> > >(_terrainHeightMap);
+
+    checkAvailableSpace();
+}
+
+EnglishFields::~EnglishFields()
+{
+
+}
+
+void EnglishFields::checkAvailableSpace()
+{
+
+}
+
+void EnglishFields::threeFieldModel()
+{
+
+}
+
+
