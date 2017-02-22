5d97eb349de9b45c5ceb0cbe431a03dafb169874
Matt Traynar <matt.traynar@gmail.com>
2017-02-22 17:37:30 +0000

    Added a new class which will be used to produce English-style fields

---
+++ include/englishfields.h
@@ -0,0 +1,14 @@
+#ifndef ENGLISHFIELDS_H
+#define ENGLISHFIELDS_H
+
+class EnglishFields
+{
+public:
+    EnglishFields();
+    ~EnglishFields();
+
+private:
+
+};
+
+#endif // ENGLISHFIELDS_H
---
+++ src/englishfields.cpp
@@ -0,0 +1,6 @@
+#include "englishfields.h"
+
+EnglishFields::EnglishFields()
+{
+
+}
