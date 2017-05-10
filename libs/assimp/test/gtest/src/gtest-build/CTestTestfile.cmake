ae5913dbd825c47b2c1c82e4ffaacff34eea93f4
Matt Traynar <matt.traynar@gmail.com>
2017-05-10 16:39:54 +0100

    Using assimp to load meshes

---
+++ libs/assimp/test/gtest/src/gtest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/i7626944/assimp/test/gtest/src/gtest
+# Build directory: /home/i7626944/assimp/test/gtest/src/gtest-build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(googlemock)
---
+++ libs/assimp/test/gtest/src/gtest-build/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/i7626944/assimp/test/gtest/src/gtest/googletest
+# Build directory: /home/i7626944/assimp/test/gtest/src/gtest-build/googlemock/gtest
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
